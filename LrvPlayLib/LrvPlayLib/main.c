ea650d8c1abe9ec545abecfa869c283392c53f38
Emmanouil Paisios <e.paissios@gmail.com>
2013-02-07 11:46:49 +0100

    Initial commit

---
+++ LrvPlayLib/LrvPlayLib/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LrvPlayLib
+//
+//  Created by Emmanouil Paisios on 2/7/13.
+//  Copyright (c) 2013 Emmanouil Paisios. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
